23db3c6f2ae1668ab71f79812ef5dc91d22aaa46
Lucas Neves <lusnowky@gmail.com>
2026-08-14 20:58:57 -0300

    Add energy demand calculation script
    This script calculates the energy demand of household appliances based on user input.

---
+++ sersEnergiaCasa.py
@@ -0,0 +1,45 @@
+# Lista de Aparelhos e seu gastos em kWh
+
+media = 0
+totalEnergia = 0
+gelad = 55
+tv = 15
+lamp = 2
+chuv = 50
+fogao = 60
+mqnLava = 15
+
+# ---------------------------------------------------
+
+# Variáveis de quantidade de itens
+
+totalItens = 0
+qtdGelad = 0
+qtdTv = 0
+qtdLamp = 0
+qtdChuv = 0
+qtdFogao = 0
+qtdMqnLava = 0
+
+# ---------------------------------------------------
+
+print("""
+===========================================================
+Bem-vindo ao seu cálculo de demanda energética da sua casa
+===========================================================""")
+
+qtdGelad = int(input("Quantas geladeiras tem em sua casa?"))
+qtdTv = int(input("Quantas TV tem em sua casa?"))
+qtdLamp = int(input("Quantas lâmpadas tem em sua casa?"))
+qtdChuv = int(input("Quantas chuveiros tem em sua casa?"))
+qtdFogao = int(input("Quantas fogão tem em sua casa?"))
+qtdMqnLava = int(input("Quantas máquinas de lavar tem em sua casa?"))
+
+totalEnergia = (gelad * qtdGelad) + (tv * qtdTv) + (lamp * qtdLamp) + (chuv * qtdChuv) + (fogao * qtdFogao) + (mqnLava * qtdMqnLava)
+totalItens = qtdGelad + qtdTv + qtdLamp + qtdChuv + qtdFogao + qtdMqnLava
+
+media = (totalEnergia / totalItens)
+print(f"A média da sua demanda energética é {media:.2f}kWh por mês")
+
+
+
